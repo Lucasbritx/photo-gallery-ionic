7ba230ec0d05f61bdfb79704a07b5445e68b61b7
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-11 22:52:39 -0300

    feat: loading photos from filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -16,8 +16,28 @@ export interface UserPhoto {
   webviewPath?: string;
 }
 
+const PHOTO_STORAGE = "photos";
 export function usePhotoGallery() {
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const { value } = await Preferences.get({ key: PHOTO_STORAGE });
+      const photosInPreferences = (
+        value ? JSON.parse(value) : []
+      ) as UserPhoto[];
+
+      for (let photo of photosInPreferences) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photosInPreferences);
+    };
+    loadSaved();
+  }, []);
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -30,6 +50,7 @@ export function usePhotoGallery() {
     const savedFileImage = await savePicture(photo, fileName);
     const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    Preferences.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
   };
 
   const savePicture = async (
